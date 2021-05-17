1033332da75f167bc9edfa1d14a67b84bb1a65eb
Nina <259290@pwr.edu.pl>
2021-05-17 20:56:14 +0200

    wektor polozenia i kat orientacji, funkcja transportowania do ukladu rodzica

---
+++ include/Prism.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "vector.hh"
+#include "../include/GeoSolid.hh"
+
+#include "size.hh"
+
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+class Prism : public GeoSolid
+{
+double KatOrient;
+Vector <3> polozenie; 
+public:
+    Prism(){};
+
+    Prism(Vector<3> pkt, double h, double w, double d);
+    Vector<3> TransDoUklRodzica(const Vector <3> &pkt1) const;
+};
+
+std::ostream &operator<<(std::ostream &Strm, const Prism &Pr);
