37ec903fe58a8b0025c86104541debca0b50824f
Nina <259290@pwr.edu.pl>
2021-05-17 20:38:13 +0200

    dodana metoda skalowania

--- include/GeoSolid.hh
+++ include/GeoSolid.hh
@@ -6,12 +6,12 @@ using namespace std;
 class GeoSolid
 {
 protected:
-/*PUnkty bryły*/
+/*Skala bryły*/
     vector<Vector<3>> pkt1;
 
 public:
     Vector<3> operator[](int Ind) const { return pkt1[Ind]; } //wypisywanie
 
     Vector<3> &operator[](int Ind) { return pkt1[Ind]; } //wpisywanie
-
+    Vector<3> Skaluj (const Vector<3> pkt1) const;
 };
