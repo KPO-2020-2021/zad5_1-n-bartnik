295712f0524707f73c732030084f225caada9c35
Nina <259290@pwr.edu.pl>
2021-05-17 20:59:24 +0200

    wektor polozenia i kat orientacji, funkcja transportowania do ukladu rodzica

--- include/Cuboid.hh
+++ include/Cuboid.hh
@@ -15,45 +15,13 @@
 using namespace std;
 class Prostopadloscian : public GeoSolid
 {
-
+Vector <3> polozenie;
+double kat_stopnie;
 
 public:
     Prostopadloscian(){};
 
     Prostopadloscian(Vector<3> pkt, double h, double w, double d);
-
-// class Cuboid
-// {
-//   Vector3D pkt1[8];
-
-// public:
-//   Cuboid(){}; /*KOnstruktor bezparametryczy*/
-//   Cuboid(Vector3D pkt, double h, double w, double r); /*KOnstruktor parametryczy*/
-//   Vector3D operator[](int Ind) const { return pkt1[Ind]; }
-//   Vector3D &operator[](int Ind) { return pkt1[Ind]; }
-//   Cuboid operator+(Vector3D wektor);
-//   double odleglosci();
-//   /*funkcja rotacji
-//   przyjmuje macierz i mnoży ją razy odpowiedni punkt
-//   aby wykonac rotacje*/
-//   void rot(Matrix3x3 mac)
-//   {
-//     for(int i=0; i<8; i++)
-//     {
-//       pkt1[i]=mac*pkt1[i];
-//     }
-//   }
-//   /*funkcja translacji wektorów
-//   przyjmuje wektor i dodaje go do aktualnego wektora*/
-//   void trans(Vector3D wektor)
-//   {
-//     *this = *this + wektor;
-//   }
-
-// };
-
-// /*
-//  Operator wypisywania, przyjmuje prostokąt
-//  */
-// std::ostream &operator<<(std::ostream &Strm,
-//                          const Cuboid &Pr);
+    Vector<3> TransDoUklRodzica(const Vector <3> &pkt1) const;
+};
+}
